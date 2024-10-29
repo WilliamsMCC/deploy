77313731452a39057cd413b5c95aaff26f561bfc
WilliamsMCC <157554642+WilliamsMCC@users.noreply.github.com>
2024-10-28 19:51:26 -0600

    cambio 7 commit

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ const getHtmlEntries = ()=>{
 export default defineConfig(
     {
         appType: 'mpa',
-        base: "/pw2-202403-vite-main/",
+        base: "/deploy/",
         build: {
             rollupOptions: {
                 input: getHtmlEntries()
